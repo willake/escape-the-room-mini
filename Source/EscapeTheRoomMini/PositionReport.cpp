fbb4bbd1be780afa6d8ab3c7982828038e5281c4
willake <tsrn46336686@gmail.com>
2019-04-09 22:29:11 +0800

    Lecture 79: Accessing Object Names

--- Source/EscapeTheRoomMini/PositionReport.cpp
+++ Source/EscapeTheRoomMini/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -20,7 +21,8 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *ObjectName);
 }
 
 
